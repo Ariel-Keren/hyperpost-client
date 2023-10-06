e0d91f8b687cf2c26bb6da219b5264bf7b2a91d4
shmil123 <arielkeren2007@gmail.com>
2023-10-06 16:00:17 +0300

    Add a title to the Hyper creation page

--- src/lib/components/createHyper/CreateHyper.svelte
+++ src/lib/components/createHyper/CreateHyper.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import createHyper from "$lib/api/createHyper";
+	import Title from "../global/Title.svelte";
 
 	let name = "";
 	let description = "";
@@ -11,34 +12,37 @@
 	};
 </script>
 
-<form on:submit={addHyper} class="flex flex-col items-center gap-10">
-	<div class="flex flex-col items-center gap-3">
-		<label for="name" class="text-gray-200 font-medium text-xl"
-			>Pick a good name for your new Hyper</label
-		>
+<div class="flex flex-col items-center gap-10">
+	<Title>Create a New Hyper</Title>
+	<form on:submit={addHyper} class="flex flex-col items-center gap-10">
+		<div class="flex flex-col items-center gap-3">
+			<label for="name" class="text-gray-200 font-medium text-xl"
+				>Pick a good name for your new Hyper</label
+			>
+			<input
+				bind:value={name}
+				placeholder="Name"
+				maxlength="35"
+				id="name"
+				class="w-52 text-lg bg-gray-950 text-gray-200 border-2 border-gray-800 rounded-lg p-1 outline-none transition-colors placeholder:text-gray-500 focus:border-gray-700"
+			/>
+		</div>
+		<div class="flex flex-col items-center gap-3">
+			<label for="description" class="text-gray-200 font-medium text-xl"
+				>Give your Hyper a short description</label
+			>
+			<textarea
+				bind:value={description}
+				placeholder="Description"
+				maxlength="100"
+				id="description"
+				class="w-96 text-lg bg-gray-950 text-gray-200 border-2 border-gray-800 rounded-lg p-2 outline-none resize-none transition-colors placeholder:text-gray-500 focus:border-gray-700"
+			/>
+		</div>
 		<input
-			bind:value={name}
-			placeholder="Name"
-			maxlength="35"
-			id="name"
-			class="w-52 text-lg bg-gray-950 text-gray-200 border-2 border-gray-800 rounded-lg p-1 outline-none transition-colors placeholder:text-gray-500 focus:border-gray-700"
+			type="submit"
+			value="Create"
+			class="text-gray-200 bg-gray-900 h-10 w-32 font-medium uppercase rounded cursor-pointer transition-colors hover:bg-gray-800"
 		/>
-	</div>
-	<div class="flex flex-col items-center gap-3">
-		<label for="description" class="text-gray-200 font-medium text-xl"
-			>Give your Hyper a short description</label
-		>
-		<textarea
-			bind:value={description}
-			placeholder="Description"
-			maxlength="100"
-			id="description"
-			class="w-96 text-lg bg-gray-950 text-gray-200 border-2 border-gray-800 rounded-lg p-2 outline-none resize-none transition-colors placeholder:text-gray-500 focus:border-gray-700"
-		/>
-	</div>
-	<input
-		type="submit"
-		value="Create"
-		class="text-gray-200 bg-gray-900 h-10 w-32 font-medium uppercase rounded cursor-pointer transition-colors hover:bg-gray-800"
-	/>
-</form>
+	</form>
+</div>
