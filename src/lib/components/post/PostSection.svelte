f8a1a901ba08625a811d8b0486977814399436b4
shmil123 <arielkeren2007@gmail.com>
2023-10-17 23:58:21 +0300

    Move the username on each post to the top

--- src/lib/components/post/PostSection.svelte
+++ src/lib/components/post/PostSection.svelte
@@ -12,12 +12,17 @@
 {#if post}
 	<div class="flex flex-col items-center gap-10">
 		<Title>{post.title}</Title>
-		<div class="flex flex-col gap-5 p-10 w-1/2 bg-gray-900 bg-opacity-50 rounded">
-			<p class="text-gray-300 text-xl whitespace-pre-wrap">{post.text}</p>
-			<p class="text-gray-500">
-				Created by <span class="text-gray-400">{post.createdBy}</span> on
-				<span class="text-gray-400">{getFormattedDate(post.createdAt)}</span>
-			</p>
+		<div class="flex flex-col w-1/2">
+			<div class="flex items-center">
+				<iconify-icon icon="ri:user-fill" class="text-green-200 text-2xl" />
+				<p class="text-gray-200 text-xl">{post.createdBy}</p>
+			</div>
+			<div class="flex flex-col gap-5 p-10 bg-gray-900 bg-opacity-50 rounded">
+				<p class="text-gray-300 text-xl whitespace-pre-wrap">{post.text}</p>
+				<p class="text-gray-500">
+					Posted on <span class="text-gray-400">{getFormattedDate(post.createdAt)}</span>
+				</p>
+			</div>
 		</div>
 		<hr class="w-1/2 border-gray-900" />
 		{#if !post.comments.length}
