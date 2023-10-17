c6b7a1a4c37917339f162ee436c3a9f54908a635
shmil123 <arielkeren2007@gmail.com>
2023-10-18 00:45:15 +0300

    Redesign the search bar

--- src/lib/components/global/Header.svelte
+++ src/lib/components/global/Header.svelte
@@ -13,16 +13,22 @@
 <header class="fixed top-0 flex items-center justify-between w-full p-5">
 	<a href="/" class="text-green-200 text-xl font-medium">hyperpost</a>
 	<div class="flex gap-3">
-		<form on:submit|preventDefault={navigateToSearchedHyper} class="relative">
+		<form
+			on:submit|preventDefault={navigateToSearchedHyper}
+			class="group/search-bar flex items-center"
+		>
+			<button
+				class="group/search-button flex justify-center items-center h-full pl-2 border-y-2 border-l-2 border-y-gray-800 border-l-gray-800 rounded-l-full transition-colors group-focus-within/search-bar:border-gray-700"
+				><iconify-icon
+					icon="carbon:search"
+					class="text-gray-500 text-xl transition-colors group-hover/search-button:text-gray-400"
+				/></button
+			>
 			<input
 				bind:value={searchedHyper}
 				placeholder="Search Hyper"
-				class="w-52 bg-gray-950 text-gray-200 border-2 border-gray-800 rounded-lg p-1 outline-none transition-colors placeholder:text-gray-500 focus:border-gray-700"
+				class="peer w-48 bg-gray-950 text-gray-200 border-y-2 border-r-2 border-y-gray-800 border-r-gray-800 rounded-r-full p-1 outline-none transition-colors placeholder:text-gray-500 group-focus-within/search-bar:border-gray-700"
 			/>
-			<button
-				class="absolute top-1 right-1 text-gray-500 text-2xl transition-colors hover:text-gray-400"
-				><iconify-icon icon="carbon:search" /></button
-			>
 		</form>
 		<a href="/create-hyper" class="flex items-center text-gray-200 text-3xl"
 			><iconify-icon icon="icon-park-outline:plus" /></a
