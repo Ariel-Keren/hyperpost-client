c8326a46c415b458d50f4b93fcb521a7008db6da
shmil123 <arielkeren2007@gmail.com>
2023-10-18 00:10:14 +0300

    Redesign PostDisplay

--- src/lib/components/hyper/PostDisplay.svelte
+++ src/lib/components/hyper/PostDisplay.svelte
@@ -8,19 +8,21 @@
 
 <a href={`/hypers/${$page.params.hyperName}/posts/${index}`}>
 	<div
-		class="flex flex-col gap-2 p-5 border-b-2 border-b-green-200 bg-gray-900 bg-opacity-50 rounded-t transition-transform hover:scale-105"
+		class="flex flex-col p-5 border-b-2 border-b-green-200 bg-gray-900 bg-opacity-50 rounded-t transition-transform hover:scale-105"
 	>
-		<h2 class="text-gray-200 font-medium">{post.title}</h2>
-		<p class="text-gray-300 whitespace-pre-wrap border-l-2 border-l-gray-800 p-2">{post.text}</p>
-		<div class="bg-gray-950 rounded py-2 px-5">
-			<p class="text-gray-500">
-				<span class="text-gray-600">Posted by</span>
-				{post.createdBy}
-			</p>
-			<p class="text-gray-500">
-				{post.comments.length}
-				<span class="text-gray-600">comment{post.comments.length !== 1 && "s"}</span>
-			</p>
+		<div class="flex items-center gap-[2px]">
+			<iconify-icon icon="mdi:user-circle" class="text-green-200 text-xl" />
+			<p class="text-gray-200 text-lg">{post.createdBy}</p>
 		</div>
+		<h2 class="text-gray-200 font-medium">{post.title}</h2>
+		<p
+			class="text-gray-300 whitespace-pre-wrap border-l-2 border-l-gray-800 p-5 mt-5 mb-1 bg-gray-950 rounded-r"
+		>
+			{post.text}
+		</p>
+		<p class="text-gray-500">
+			{post.comments.length}
+			<span class="text-gray-600">comment{post.comments.length !== 1 && "s"}</span>
+		</p>
 	</div>
 </a>
