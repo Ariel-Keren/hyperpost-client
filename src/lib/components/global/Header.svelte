fe8b4a1ac7ec4c864442f621a1a008b726c31c03
shmil123 <arielkeren2007@gmail.com>
2023-10-18 01:08:56 +0300

    Create a component for the search bar

--- src/lib/components/global/Header.svelte
+++ src/lib/components/global/Header.svelte
@@ -1,40 +1,12 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
 	import "iconify-icon";
-
-	let searchInputRef: HTMLInputElement | null = null;
-	let searchedHyper = "";
-
-	const navigateToSearchedHyper = () => {
-		if (!searchInputRef) return;
-
-		searchInputRef.focus();
-		if (searchedHyper.replaceAll(" ", "") === "") return;
-		goto(`/hypers/${searchedHyper}`);
-	};
+	import SearchBar from "./SearchBar.svelte";
 </script>
 
 <header class="fixed top-0 flex items-center justify-between w-full p-5">
 	<a href="/" class="text-green-200 text-xl font-medium">hyperpost</a>
 	<div class="flex gap-3">
-		<form
-			on:submit|preventDefault={navigateToSearchedHyper}
-			class="group/search-bar flex items-center"
-		>
-			<button
-				class="group/search-button flex justify-center items-center h-full pl-2 border-y-2 border-l-2 border-y-gray-800 border-l-gray-800 rounded-l-full outline-none transition-colors group-focus-within/search-bar:border-gray-700"
-				><iconify-icon
-					icon="carbon:search"
-					class="text-gray-500 text-xl transition-colors group-hover/search-button:text-gray-400"
-				/></button
-			>
-			<input
-				bind:value={searchedHyper}
-				bind:this={searchInputRef}
-				placeholder="Search Hyper"
-				class="peer w-28 bg-gray-950 text-gray-200 border-y-2 border-r-2 border-y-gray-800 border-r-gray-800 rounded-r-full p-1 outline-none transition-all placeholder:text-gray-500 group-focus-within/search-bar:border-gray-700 group-focus-within/search-bar:w-48"
-			/>
-		</form>
+		<SearchBar />
 		<a href="/create-hyper" class="flex items-center text-gray-200 text-3xl"
 			><iconify-icon icon="icon-park-outline:plus" /></a
 		>
---
+++ src/lib/components/global/SearchBar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+	let searchInputRef: HTMLInputElement | null = null;
+	let searchedHyper = "";
+
+	const navigateToSearchedHyper = () => {
+		if (!searchInputRef) return;
+
+		searchInputRef.focus();
+		if (searchedHyper.replaceAll(" ", "") === "") return;
+		goto(`/hypers/${searchedHyper}`);
+	};
+</script>
+
+<form on:submit|preventDefault={navigateToSearchedHyper} class="group/search-bar flex items-center">
+	<button
+		class="group/search-button flex justify-center items-center h-full pl-2 border-y-2 border-l-2 border-y-gray-800 border-l-gray-800 rounded-l-full outline-none transition-colors group-focus-within/search-bar:border-gray-700"
+		><iconify-icon
+			icon="carbon:search"
+			class="text-gray-500 text-xl transition-colors group-hover/search-button:text-gray-400"
+		/></button
+	>
+	<input
+		bind:value={searchedHyper}
+		bind:this={searchInputRef}
+		placeholder="Search Hyper"
+		class="peer w-28 bg-gray-950 text-gray-200 border-y-2 border-r-2 border-y-gray-800 border-r-gray-800 rounded-r-full p-1 outline-none transition-all placeholder:text-gray-500 group-focus-within/search-bar:border-gray-700 group-focus-within/search-bar:w-48"
+	/>
+</form>
