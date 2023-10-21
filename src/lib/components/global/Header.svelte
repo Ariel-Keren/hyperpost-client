22177cf3b224249caee15ec545848178ff006348
shmil123 <arielkeren2007@gmail.com>
2023-10-21 04:02:11 +0300

    Add logo

--- src/lib/components/global/Header.svelte
+++ src/lib/components/global/Header.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import SearchBar from "./SearchBar.svelte";
+	import logo from "$lib/logo.svg";
 </script>
 
 <header class="fixed top-0 flex items-center justify-between w-full p-5">
-	<a href="/" class="text-primary text-xl font-medium transition-colors hover:text-primary-hover"
-		>hyperpost</a
+	<a
+		href="/"
+		class="flex items-center gap-1 rounded p-2 text-white text-xl transition-colors hover:bg-dark"
+		><img src={logo} width="35" alt="" />hyperpost</a
 	>
 	<SearchBar />
 </header>
